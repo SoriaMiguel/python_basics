ffacfefdebe682c106f0bd82467beaa5f2e86387
SoriaMiguel <soria.miguel.8@gmail.com>
2017-06-22 13:56:51 -0400

    little more

--- pets.py
+++ pets.py
@@ -10,7 +10,11 @@ print(pets)
 
 # positional arguments
 
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name,animal_type='dog'):
+    # a pet name must always be given but unless otherwise specified, the devault type will be dog
     """Display information about a pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet(pet_name='willie')
+# calling style can be either "pet_name='willie'" or just 'willie', it doesn't matter
